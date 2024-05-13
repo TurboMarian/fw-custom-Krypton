a73e0de3d3331b45307b16e68d766070c4ca7492
TurboMarian <podgajnymariusz@gmail.com>
2024-05-13 20:18:12 +0100

    Update board.mk

--- board.mk
+++ board.mk
@@ -1,15 +1,21 @@
-BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp \
-    $(BOARD_DIR)/default_tune.cpp \
+# List of all the board related files.
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
+# Override DEFAULT_ENGINE_TYPE
+DDEFS += -DSTM32F407xx
+DDEFS += -DFIRMWARE_ID=\"KRYPTON\"
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::Unassigned
+DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
+DDEFS += -DHAL_VSS_USE_PAL=TRUE
 
-BOARDINC += $(BOARD_DIR)/generated/controllers/generated
+# TS_SECONDARY_UxART_PORT
+DDEFS += -DSTM32_SERIAL_USE_USART3=TRUE
+DDEFS += -DTS_SECONDARY_UxART_PORT=SD3 -DEFI_TS_SECONDARY_IS_SERIAL=TRUE
+DDEFS += -DEFI_AUX_SERIAL=FALSE
 
-# defines SHORT_BOARD_NAME
-include $(BOARD_DIR)/meta-info.env
+# Enable specific features for the board
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
 
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
-
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
-# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
