0e1473a5d5e4b50397bc7716fc022a83db3ecd47
GitHub git update Action <action@github.com>
2024-06-19 16:44:22 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_KRYPTON.h
+++ generated/controllers/generated/signature_KRYPTON.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3125504551
-#define TS_SIGNATURE "rusEFI main.2024.06.18.KRYPTON.3125504551"
+#define SIGNATURE_HASH 3412260117
+#define TS_SIGNATURE "rusEFI main.2024.06.19.KRYPTON.3412260117"
